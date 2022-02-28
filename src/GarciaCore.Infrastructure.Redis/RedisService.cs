9cdbf49cd06915af6e9a4653301c9275a203c227
SeckinPullu <seckinpullu@hotmail.com>
2022-02-28 12:41:52 +0300

    redis implemented

---
+++ src/GarciaCore.Infrastructure.Redis/RedisService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace GarciaCore.Infrastructure.Redis
+{
+    public class RedisService : IRedisService
+    {
+        private readonly IConnectionMultiplexer _connection;
+        private readonly RedisConnectionFactory _connectionFactory;
+
+        public RedisService(RedisConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+            _connection = _connectionFactory.GetConnection();
+        }
+
+        public async Task PublishAsync(string channel, string message)
+        {
+            await _connection.GetSubscriber().PublishAsync(channel, message);
+        }
+
+        public async Task SubscribeAsync(string channel, Action<string> action)
+        {
+            await _connection.GetSubscriber().SubscribeAsync(channel, (ch, message) =>
+                action(message)
+            );
+        }
+
+        public async Task ExecuteWithLockAsync(Action action, int expiryInMilliSeconds)
+        {
+            string key = action.Method.IsSpecialName ? action.Method.Name : _connectionFactory.GetServiceLockKey();
+            TimeSpan expiration = new TimeSpan(0, 0, 0, 0, expiryInMilliSeconds);
+            bool locked = false;
+
+            while (!locked)
+            {
+                locked = await GetLockAsync(key, expiration);
+                if (locked)
+                {
+                    await Task.Run(action);
+                    await ReleaseLockAsync(key);
+                }
+            }
+
+        }
+
+        public async Task<bool> AcquireLockAsync(string key, int expiryInMilliSeconds) => await GetLockAsync(key, new TimeSpan(0, 0, 0, 0, expiryInMilliSeconds));
+
+        public void Dispose()
+        {
+            _connectionFactory.DisposeConnection();
+        }
+
+        private async Task<bool> GetLockAsync(string key, TimeSpan expiration)
+        {
+            return await _connection.GetDatabase()
+                .StringSetAsync(key, Guid.NewGuid().ToString(), expiration, When.NotExists);
+        }
+
+        private async Task ReleaseLockAsync(string key)
+        {
+            await _connection.GetDatabase().KeyDeleteAsync(key);
+        }
+    }
+}
---
+++ src/GarciaCore.Infrastructure/IAsyncPubSubService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarciaCore.Infrastructure
+{
+    public interface IAsyncPubSubService : IDisposable
+    {
+        /// <summary>
+        /// Publishes a message to the related channel
+        /// </summary>
+        /// <param name="channel">Name of the related channel.</param>
+        /// <param name="message">A message to be published</param>
+        /// <returns></returns>
+        Task PublishAsync(string channel, string message);
+        /// <summary>
+        /// Subscribe to perform some operation when a change to the preferred/active node
+        /// is broadcast.
+        /// </summary>
+        /// <param name="channel">Name of the related channel.</param>
+        /// <param name="action">An action that processes the received message</param>
+        /// <returns></returns>
+        Task SubscribeAsync(string channel, Action<string> action);
+    }
+}
