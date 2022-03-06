d937488d97691d4519afb9bb9e4e089ed883607a
DESKTOP-QMFIQRF\vehbi <vehbi.yurdakurban@outlook.com>
2022-03-06 13:43:01 +0300

    Authentication ve jwtservice added.

---
+++ src/GarciaCore.Application/Contracts/Identity/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GarciaCore.Application.Contracts.Identity
+{
+    public interface IJwtService
+    {
+        Task<string> GenerateJwt(string userName, string id, List<string> roles);
+    }
+}
