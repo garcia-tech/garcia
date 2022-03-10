5ff0d4f1d70f88986852bce38a1b413b855620b1
SeckinPullu <seckinpullu@hotmail.com>
2022-03-10 13:20:40 +0300

    entityframework features added

---
+++ src/GarciaCore.Persistence.EntityFramework.PostgreSQL/EntityFrameworkPostgreSqlRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace GarciaCore.Persistence.EntityFramework.PostgreSql
+{
+    public static class EntityFrameworkPostgreSqlRegistration
+    {
+        public static IServiceCollection AddEfCorePostgreSql<TContext,TOptions>(this IServiceCollection services, IOptions<TOptions> settings) 
+            where TOptions : EfCoreSettings
+            where TContext : BaseContext
+        {
+            return services.AddDbContext<TContext>(options => options.UseNpgsql(settings.Value.ConnectionString,
+                x => x.MigrationsAssembly(settings.Value.MigrationsAssembly)));
+        }
+    }
+}
---
+++ src/GarciaCore.Persistence.EntityFramework/EfCoreSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GarciaCore.Infrastructure;
+
+namespace GarciaCore.Persistence.EntityFramework
+{
+    public class EfCoreSettings : DatabaseSettings
+    {
+        public string MigrationsAssembly { get; set; }
+    }
+}
