c99b4c578b1d3db76bf292eeeeb1089a77e97acf
SeckinPullu <seckinpullu@hotmail.com>
2022-03-16 16:12:28 +0300

    base structures (BaseService, BaseController, BaseResponse) created and modified

---
+++ tests/unit/GarciaCore.Application.Tests/TestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarciaCore.Application.Tests
+{
+    public class TestDto
+    {
+        public string Name { get; set; }
+    }
+}
