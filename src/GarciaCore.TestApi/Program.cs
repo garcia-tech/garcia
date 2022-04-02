9123c20ce7ea6b8b9ad742511714568b54d71296
DESKTOP-QMFIQRF\vehbi <vehbi.yurdakurban@outlook.com>
2022-04-02 12:59:47 +0300

    Fix

---
+++ src/GarciaCore.TestApi/Program.cs
@@ -0,0 +1,32 @@
+using GarciaCore.Application.Contracts.Identity;
+using GarciaCore.Infrastructure.Identity;
+using GarciaCore.TestApi.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IJwtService, JwtService>();
+builder.Services.AddJwtOptions(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
