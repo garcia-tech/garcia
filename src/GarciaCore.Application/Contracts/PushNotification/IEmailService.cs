caf5d9bd97f54506895157bcc93250a0ecdd831d
DESKTOP-QMFIQRF\vehbi <vehbi.yurdakurban@outlook.com>
2022-04-04 19:34:53 +0300

    Pushnotification & Firebase added.

---
+++ src/GarciaCore.Application/Contracts/PushNotification/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarciaCore.Application.Contracts.PushNotification
+{
+    public interface IPushNotificationService
+    {
+        Task<int> SendPushNotificationAsync(string token, Dictionary<string, string> data);
+        Task<int> SendPushNotificationAsync(List<string> tokens, Dictionary<string, string> data);
+        Task<int> SendBatchPushNotificationAsync(string token, List<Dictionary<string, string>> data);
+    }
+}
---
+++ src/GarciaCore.Infrastructure.PushNotification.Firebase/FirebasePushNotificationServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using GarciaCore.Application.Contracts.Email;
+using GarciaCore.Application.Contracts.Marketing;
+using GarciaCore.Application.Contracts.PushNotification;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GarciaCore.Infrastructure.PushNotification.Firebase
+{
+    public static class FirebasePushNotificationServiceRegistration
+    {
+        public static IServiceCollection AddMandrillEmailService(this IServiceCollection services, FirebasePushNotificationSettings settings)
+        {
+            services.Configure<FirebasePushNotificationSettings>(options =>
+            {
+                options.AccessToken = settings.AccessToken;
+            });
+
+            services.AddScoped<IPushNotificationService, FirebasePushNotificationService>();
+            return services;
+        }
+
+        public static IServiceCollection AddMandrillEmailService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<FirebasePushNotificationSettings>(options =>
+            {
+                options.AccessToken = configuration[$"{nameof(FirebasePushNotificationSettings)}:{nameof(options.AccessToken)}"];
+            });
+
+            services.AddScoped<IPushNotificationService, FirebasePushNotificationService>();
+            return services;
+        }
+    }
+}
