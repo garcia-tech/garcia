9b08f4c99d5089daa95ae905fe025c95125b44ca
Seçkin PÜLLÜ <Seckinpullu@hotmail.com>
2022-04-19 16:13:27 +0300

    summaries added

--- src/GarciaCore.Infrastructure.ElasticSearch/ElasticSearchServiceRegistration.cs
+++ src/GarciaCore.Infrastructure.ElasticSearch/ElasticSearchServiceRegistration.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GarciaCore.Application.ElasticSearch.Contracts;
+﻿using GarciaCore.Application.ElasticSearch.Contracts;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GarciaCore.Infrastructure.ElasticSearch
