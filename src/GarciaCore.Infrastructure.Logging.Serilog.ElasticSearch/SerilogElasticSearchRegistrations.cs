3249d8c5c2059c08a6a0e3e189bfe8c82ce9df0f
Seçkin PÜLLÜ <Seckinpullu@hotmail.com>
2022-04-21 09:18:55 +0300

    serilog registrations created

---
+++ src/GarciaCore.Infrastructure.Logging.Serilog.ElasticSearch/SerilogElasticSearchRegistrations.cs
@@ -0,0 +1,63 @@
+﻿using GarciaCore.Infrastructure.Logging.Serilog.Configurations;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Filters;
+using Serilog.Sinks.Elasticsearch;
+
+namespace GarciaCore.Infrastructure.Logging.Serilog.ElasticSearch
+{
+    public static class SerilogElasticSearchRegistrations
+    {
+        public static ILoggingBuilder AddGarciaSerilogElastic(this ILoggingBuilder logging, ElasticsearchSinkOptions options, LogEventLevel minimumLevel = LogEventLevel.Debug, int bufferSize = 1000, bool blockWhenFull = false, bool logConsole = false, params CustomProperty[] customProperties)
+        {
+            var loggerConfigurations = new LoggerConfiguration()
+                .AddCustomProperties(customProperties)
+                .MinimumLevel.Is(minimumLevel)
+                .Enrich.FromLogContext()
+                .Filter.ByExcluding(Matching.FromSource("System"))
+                .WriteTo.Async(c => c.Elasticsearch(options), bufferSize, blockWhenFull);
+
+            if(logConsole)
+            {
+                loggerConfigurations.WriteTo.Async(c => c.Console(), bufferSize, blockWhenFull);
+            }
+
+            var logger = loggerConfigurations.CreateLogger();
+            return logging.ClearProviders().AddSerilog(logger);
+        }
+
+        public static ILoggingBuilder AddGarciaSerilogElastic(this ILoggingBuilder logging, string connectionString, string indexFormat, string username = null, string password = null, LogEventLevel minimumLevel = LogEventLevel.Debug, int bufferSize = 1000, bool blockWhenFull = false, bool logConsole = false, params CustomProperty[] customProperties)
+        {
+            var options = new ElasticsearchSinkOptions(new Uri(connectionString))
+            {
+                AutoRegisterTemplate = true,
+                OverwriteTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                NumberOfReplicas = 1,
+                NumberOfShards = 2,
+                IndexFormat = indexFormat,
+                FailureCallback = e => Console.WriteLine(e.Exception)
+            };
+
+            if(!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                options.ModifyConnectionSettings = x => x.BasicAuthentication(username, password);
+            }
+
+            var loggerConfigurations = new LoggerConfiguration()
+                .AddCustomProperties(customProperties)
+                .MinimumLevel.Is(minimumLevel)
+                .Enrich.FromLogContext()
+                .Filter.ByExcluding(Matching.FromSource("System"))
+                .WriteTo.Async(c => c.Elasticsearch(options), bufferSize, blockWhenFull);
+
+            if (logConsole)
+            {
+                loggerConfigurations.WriteTo.Async(c => c.Console(), bufferSize, blockWhenFull);
+            }
+            var logger = loggerConfigurations.CreateLogger();
+            return logging.ClearProviders().AddSerilog(logger);
+        }
+    }
+}
---
+++ src/GarciaCore.Infrastructure.Logging.Serilog.Graylog/SerilogGraylogRegistrations.cs
@@ -0,0 +1,62 @@
+﻿using GarciaCore.Infrastructure.Logging.Serilog.Configurations;
+using Microsoft.Extensions.Logging;
+using Serilog.Events;
+using Serilog.Filters;
+using Serilog;
+using Serilog.Sinks.Graylog;
+using Serilog.Sinks.Graylog.Core.Transport;
+
+namespace GarciaCore.Infrastructure.Logging.Serilog.Graylog
+{
+    public static class SerilogGraylogRegistrations
+    {
+        public static ILoggingBuilder AddGarciaSerilogGraylog(this ILoggingBuilder logging, GraylogSinkOptions options, LogEventLevel minimumLevel = LogEventLevel.Debug, int bufferSize = 1000, bool blockWhenFull = false, bool logConsole = false, params CustomProperty[] customProperties)
+        {
+            var loggerConfigurations = new LoggerConfiguration()
+                .AddCustomProperties(customProperties)
+                .MinimumLevel.Is(minimumLevel)
+                .Enrich.FromLogContext()
+                .Filter.ByExcluding(Matching.FromSource("System"))
+                .WriteTo.Async(c => c.Graylog(options), bufferSize, blockWhenFull);
+
+            if (logConsole)
+            {
+                loggerConfigurations.WriteTo.Async(c => c.Console(), bufferSize, blockWhenFull);
+            }
+
+            var logger = loggerConfigurations.CreateLogger();
+            return logging.ClearProviders().AddSerilog(logger);
+        }
+
+        public static ILoggingBuilder AddGarciaSerilogGraylog(this ILoggingBuilder logging, string hostNameOrAddress, int port,string? username = null, string? password = null, LogEventLevel minimumLevel = LogEventLevel.Debug, int bufferSize = 1000, bool blockWhenFull = false, bool logConsole = false, params CustomProperty[] customProperties)
+        {
+            var options = new GraylogSinkOptions
+            {
+                HostnameOrAddress = hostNameOrAddress,
+                Port = port,
+                TransportType = TransportType.Http
+
+            };
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                options.PasswordInHttp = password;
+                options.UsernameInHttp = username;
+            }
+
+            var loggerConfigurations = new LoggerConfiguration()
+                .AddCustomProperties(customProperties)
+                .MinimumLevel.Is(minimumLevel)
+                .Enrich.FromLogContext()
+                .Filter.ByExcluding(Matching.FromSource("System"))
+                .WriteTo.Async(c => c.Graylog(options), bufferSize, blockWhenFull);
+
+            if (logConsole)
+            {
+                loggerConfigurations.WriteTo.Async(c => c.Console(), bufferSize, blockWhenFull);
+            }
+            var logger = loggerConfigurations.CreateLogger();
+            return logging.ClearProviders().AddSerilog(logger);
+        }
+    }
+}
