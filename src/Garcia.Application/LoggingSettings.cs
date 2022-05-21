0695349f67aab1a910978325eb47c45f54c2995f
DESKTOP-QMFIQRF\vehbi <vehbi.yurdakurban@outlook.com>
2022-05-21 12:43:49 +0300

    Logging settings added.

---
+++ src/Garcia.Application/LoggingSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Garcia.Application
+{
+    public class Logging
+    {
+        public Loglevel LogLevel { get; set; }
+    }
+
+    public class Loglevel
+    {
+        public string Default { get; set; }
+        public string Microsoft { get; set; }
+        [JsonPropertyName("Microsoft.Hosting.Lifetime")]
+        public string MicrosoftHostingLifetime { get; set; }
+    }
+}
---
+++ src/Garcia.Infrastructure.Logging.Serilog/SerilogSettings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Garcia.Infrastructure.Logging.Serilog
+{
+    public class Serilog
+    {
+        public string[] Using { get; set; }
+        public string MinimumLevel { get; set; }
+        public List<Writeto> WriteTo { get; set; }
+        public Properties Properties { get; set; }
+        public List<Filter> Filter { get; set; }
+    }
+
+    public class Properties
+    {
+        public string Application { get; set; }
+    }
+
+    public class Writeto
+    {
+        public string Name { get; set; }
+        public WriteToArgs Args { get; set; }
+    }
+
+    public class WriteToArgs
+    {
+        [JsonPropertyName("hostnameOrAddress")]
+        public string HostnameOrAddress { get; set; }
+        [JsonPropertyName("port")]
+        public string Port { get; set; }
+        [JsonPropertyName("transportType")]
+        public string TransportType { get; set; }
+    }
+
+    public class Filter
+    {
+        public string Name { get; set; }
+        public FilterArgs Args { get; set; }
+    }
+
+    public class FilterArgs
+    {
+        [JsonPropertyName("expression")]
+        public string Expression { get; set; }
+    }
+}
