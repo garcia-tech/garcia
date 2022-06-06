ec0dcc7ee17582ba7fb7b475bf8b39dd59b4b635
DESKTOP-QMFIQRF\vehbi <vehbi.yurdakurban@outlook.com>
2022-06-06 17:01:51 +0300

    AddGarciaLocalization

--- src/Garcia.Infrastructure.Localization.Local/LocalizationServiceRegistrations.cs
+++ src/Garcia.Infrastructure.Localization.Local/LocalizationServiceRegistrations.cs
@@ -10,7 +10,7 @@ namespace Garcia.Infrastructure.Localization.Local
 {
     public static class LocalizationServiceRegistrations
     {
-        public static IServiceCollection AddLocalization<T>(this IServiceCollection services)
+        public static IServiceCollection AddGarciaLocalization<T>(this IServiceCollection services)
             where T : class, ILocalizationItemService
         {
             services.AddScoped<ILocalizationItemService, T>();
@@ -18,14 +18,14 @@ namespace Garcia.Infrastructure.Localization.Local
             return services;
         }
 
-        public static IServiceCollection AddLocalization(this IServiceCollection services)
+        public static IServiceCollection AddGarciaLocalization(this IServiceCollection services)
         {
             services.AddScoped<ILocalizationItemService, LocalizationItemService>();
             services.AddScoped<ILocalizationService, LocalizationService>();
             return services;
         }
 
-        public static IServiceCollection AddLocalization<TService, TItemServive>(this IServiceCollection services)
+        public static IServiceCollection AddGarciaLocalization<TService, TItemServive>(this IServiceCollection services)
             where TItemServive : class, ILocalizationItemService
             where TService : class, ILocalizationService
         {
