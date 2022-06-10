ba9bc77b1f333f62feb6c8f7327f691f2f65d93d
Seçkin PÜLLÜ <Seckinpullu@hotmail.com>
2022-06-10 10:45:32 +0300

    loggedinuser model created

---
+++ src/Garcia.Application/LoggedInUser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Garcia.Application.Contracts.Identity;
+
+namespace Garcia.Application
+{
+    public class LoggedInUser : IUser
+    {
+        public string Username { get; set; }
+        public string Id { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
