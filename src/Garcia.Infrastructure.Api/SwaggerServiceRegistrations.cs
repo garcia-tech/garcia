b8265f94f7ead91a70517acf86523a018b310f0e
Seçkin PÜLLÜ <Seckinpullu@hotmail.com>
2022-06-20 15:37:21 +0300

    AddSwaggerWithAuthorization registration method created

---
+++ src/Garcia.Infrastructure.Api/SwaggerServiceRegistrations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Garcia.Infrastructure.Api
+{
+    public static class SwaggerServiceRegistrations
+    {
+        public static IServiceCollection AddSwaggerWithAuthorization(this IServiceCollection services)
+        {
+            return services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme.",
+                        Type = SecuritySchemeType.Http, 
+                        Scheme = "bearer" 
+                    });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+    }
+}
