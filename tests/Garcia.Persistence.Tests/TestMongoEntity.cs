2e3da12e873e6cc27475f064e81c1863f7a25200
Seçkin PÜLLÜ <Seckinpullu@hotmail.com>
2022-07-28 19:26:41 +0300

    mongodb autocaching created

--- tests/Garcia.Persistence.Tests/TestMongoEntity.cs
+++ tests/Garcia.Persistence.Tests/TestMongoEntity.cs
@@ -9,6 +9,12 @@ namespace Garcia.Persistence.Tests
 {
     public class TestMongoEntity : MongoDbEntity
     {
+        public TestMongoEntity()
+        {
+            CacheExpirationInMinutes = 2;
+            CachingEnabled = true;
+        }
+
         public string Name { get; set; }
         public int Indicator { get; set; }
     }
