4122cf5e73188301436922ecf18d94269a185167
Seçkin PÜLLÜ <Seckinpullu@hotmail.com>
2022-09-28 17:10:19 +0300

    GarciaCacheInstanceMissingException created

---
+++ src/Garcia.Exceptions.Persistence/GarciaCacheInstanceMissingException.cs
@@ -0,0 +1,11 @@
+﻿namespace Garcia.Exceptions.Persistence
+{
+    public class GarciaCacheInstanceMissingException : Exception
+    {
+        public GarciaCacheInstanceMissingException() : base("GarciaCache instance cannot be found.",
+            new Exception("If entity caching enabled you must register GarciaCache instance and pass as a parameter to the constructor from inherited class.")
+            )
+        {
+        }
+    }
+}
