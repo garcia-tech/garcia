72e9b1792bd0455add10dcfe73936c974ae801f5
Seçkin PÜLLÜ <Seckinpullu@hotmail.com>
2022-11-11 19:30:37 +0300

    AddBaseService added to ApplicationServiceRegistrations

--- src/Garcia.Application/ApplicationServiceRegistrations.cs
+++ src/Garcia.Application/ApplicationServiceRegistrations.cs
@@ -1,6 +1,21 @@
-﻿namespace Garcia.Application
+﻿using Garcia.Application.Contracts.Persistence;
+using Garcia.Application.Services;
+using Garcia.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Garcia.Application
 {
     public static class ApplicationServiceRegistrations
     {
+        public static IServiceCollection AddBaseService<TRepository, TEntity, TDto, TKey>(this IServiceCollection serivces)
+            where TRepository : IAsyncRepository<TEntity, TKey>
+            where TKey : struct, IEquatable<TKey>
+            where TEntity : Entity<TKey>
+            where TDto : class
+        {
+            return serivces.AddScoped<IBaseService<TEntity, TDto, TKey>,
+                BaseService<TRepository, TEntity, TDto, TKey>>();
+        }
     }
 }
