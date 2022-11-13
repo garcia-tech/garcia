6d912e9797b062eb4d0a62038630cf7863e01894
Seçkin PÜLLÜ <Seckinpullu@hotmail.com>
2022-11-13 22:51:30 +0300

    fix: AddAuthenticationService moved from application.identity to infrastructure.identity

---
+++ src/Garcia.Infrastructure.Identity/IdentityServiceRegistrations.cs
@@ -0,0 +1,40 @@
+﻿using Garcia.Application.Contracts.Identity;
+using Garcia.Application.Contracts.Persistence;
+using Garcia.Application.Identity.Services;
+using Garcia.Domain.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Garcia.Infrastructure.Identity
+{
+    public static class IdentityServiceRegistrations
+    {
+        public static IServiceCollection AddAuthenticationService<TRepository, TUser, TUserDto, TKey>(this IServiceCollection services, IConfiguration configuration)
+            where TRepository : IAsyncRepository<TUser, TKey>
+            where TKey : IEquatable<TKey>
+            where TUser : class, IUserEntity<TKey>
+            where TUserDto : class, IUser
+        {
+            return services
+                .AddEncryption()
+                .AddJwtService()
+                .AddJwtOptions(configuration)
+                .AddScoped<IAuthenticationService<TUser, TUserDto, TKey>,
+                    AuthenticationService<TRepository, TUser, TUserDto, TKey>>();
+        }
+
+        public static IServiceCollection AddAuthenticationService<TRepository, TUser, TUserDto, TKey>(this IServiceCollection services, JwtIssuerOptions jwtOptions)
+            where TRepository : IAsyncRepository<TUser, TKey>
+            where TKey : IEquatable<TKey>
+            where TUser : class, IUserEntity<TKey>
+            where TUserDto : class, IUser
+        {
+            return services
+                .AddEncryption()
+                .AddJwtService()
+                .AddJwtOptions(jwtOptions)
+                .AddScoped<IAuthenticationService<TUser, TUserDto, TKey>,
+                    AuthenticationService<TRepository, TUser, TUserDto, TKey>>();
+        }
+    }
+}
